6240c4a7cf68dc39243d3ee12597803ab92dc60f
Will Woods <wwoods@redhat.com>
2017-10-09 19:34:34 -0400

    Digest: add ObjectDigest, DigestContext, etc.
    This commit adds new types for handling object digests and digest
    algorithms.
    
    ObjectDigest is a type for binary digests - it's an alias for the Bytes
    type used internally by Crypto.Hash.Digest (from Data.ByteArray, which
    is why we now depend on `memory`.)
    
    DigestContext is the context for a particular DigestAlgorithm, which can
    be used in lazy piplines and conduits and such.
    
    DigestAlgorithm is a data type corresponding to (surprise!) a particular
    HashAlgorithm.
    
    This allows us to do the lookup of the DigestAlgorithm once, in
    openContentStore, and return an error if we don't recognize the
    requested hash algorithm. Which means the signature for our hasher
    function is now Maybe-less, since we don't have to worry about whether
    the hash algorithm exists or not. Hooray!
    
    This commit also modifies ContentStore so that we're using (binary)
    ObjectDigests internally wherever possible, and converting to string/hex
    represenatation when needed. This is probably a good idea since
    a) the API probably shouldn't take arbitrary strings as input, and
    b) binary ObjectDigests take up half as much data, and so should be our
    preferred representation for space-saving and speed purposes.
    
    We also add a (small) test suite to make sure digests work as expected.

--- Data/ContentStore/Digest.hs
+++ Data/ContentStore/Digest.hs
@@ -1,38 +1,113 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ExistentialQuantification #-}
 
-module Data.ContentStore.Digest(ObjectDigest(..),
-                                hashByteString,
-                                hashLazyByteString)
+module Data.ContentStore.Digest(DigestAlgorithm,
+                                getDigestAlgorithm,
+                                digestName,
+                                digestSize,
+                                digestInit,
+                                DigestContext,
+                                digestUpdate,
+                                digestUpdates,
+                                digestFinalize,
+                                digestByteString,
+                                digestLazyByteString,
+                                ObjectDigest,
+                                toHex,
+                                fromByteString)
  where
 
-import           Crypto.Hash(Digest, hash, hashlazy)
-import           Crypto.Hash.Algorithms(SHA256, SHA512)
+import           Crypto.Hash
+import           Data.ByteArray(Bytes, ByteArrayAccess, convert)
+import           Data.ByteArray.Encoding(Base(..), convertToBase)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Text as T
+import           Data.Text.Encoding(decodeUtf8)
 
--- Only a subset of the algorithms provided by cryptonite are supported
--- by our content store.  You can't have everything.  Because each
--- algorithm is implemented as a different type (though, all are
--- members of the HashAlgorithm typeclass), we need to wrap them up into
--- a single data type if we are to write generic hashing functions.  And
--- we want to do that to prevent the spread of knowing which algorithm
--- is in use throughout the code.
-data ObjectDigest = ObjectSHA256 (Digest SHA256)
-                  | ObjectSHA512 (Digest SHA512)
+-- cryptonite is about 50% too clever with its types, if you ask me.
+-- Here's simpler ones that we can use in ContentStore.
+
+--
+-- DIGEST TYPES
+--
+
+-- | A DigestAlgorithm represents one specific hash algorithm.
+data DigestAlgorithm = forall a. (HashAlgorithm a, Show a) => DigestAlgorithm a
+
+-- | Holds the context for a given instance of a DigestAlgorithm.
+data DigestContext = forall a. HashAlgorithm a => DigestContext (Context a)
+
+-- ObjectDigest is the (binary) representation of a digest.
+newtype ObjectDigest = ObjectDigest Bytes
+    deriving (Eq, Ord, ByteArrayAccess)
 
 instance Show ObjectDigest where
-    show (ObjectSHA256 d) = show d
-    show (ObjectSHA512 d) = show d
+    show (ObjectDigest b) = show b
 
-hashByteString :: T.Text -> BS.ByteString -> Maybe ObjectDigest
-hashByteString algo bs = case algo of
-    "SHA256" -> Just $ ObjectSHA256 (hash bs :: Digest SHA256)
-    "SHA512" -> Just $ ObjectSHA512 (hash bs :: Digest SHA512)
-    _        -> Nothing
+--
+-- PRIVATE FUNCTIONS
+--
 
-hashLazyByteString :: T.Text -> LBS.ByteString -> Maybe ObjectDigest
-hashLazyByteString algo lbs = case algo of
-    "SHA256" -> Just $ ObjectSHA256 (hashlazy lbs :: Digest SHA256)
-    "SHA512" -> Just $ ObjectSHA512 (hashlazy lbs :: Digest SHA512)
-    _        -> Nothing
+fromDigest :: HashAlgorithm a => Digest a -> ObjectDigest
+fromDigest = ObjectDigest . convert
+
+hashLazyWith :: HashAlgorithm a => a -> LBS.ByteString -> Digest a
+hashLazyWith _ = hashlazy
+
+--
+-- PUBLIC FUNCTIONS
+--
+
+-- | Get the name of this DigestAlgorithm
+digestName :: DigestAlgorithm -> String
+digestName (DigestAlgorithm a) = show a
+
+-- | The size of the ObjectDigest returned by this DigestAlgorithm
+digestSize :: DigestAlgorithm -> Int
+digestSize (DigestAlgorithm a) = hashDigestSize a
+
+-- | Initialize a new DigestContext for this DigestAlgorithm
+digestInit :: DigestAlgorithm -> DigestContext
+digestInit (DigestAlgorithm a) = DigestContext $ hashInitWith a
+
+-- | Update the DigestContext with one ByteString/Bytes/etc item
+digestUpdate :: ByteArrayAccess ba => DigestContext -> ba -> DigestContext
+digestUpdate (DigestContext ctx) = DigestContext . hashUpdate ctx
+
+-- | Update the DigestContext with many ByteString/Bytes/etc. items
+digestUpdates :: ByteArrayAccess ba => DigestContext -> [ba] -> DigestContext
+digestUpdates (DigestContext ctx) = DigestContext . hashUpdates ctx
+
+-- | Finish the digest, returning an ObjectDigest
+digestFinalize :: DigestContext -> ObjectDigest
+digestFinalize (DigestContext ctx) = fromDigest $ hashFinalize ctx
+
+-- | Hash a ByteString into an ObjectDigest
+digestByteString :: DigestAlgorithm -> BS.ByteString -> ObjectDigest
+digestByteString (DigestAlgorithm a) = fromDigest . hashWith a
+
+-- | Hash a lazy ByteString into an ObjectDigest
+digestLazyByteString :: DigestAlgorithm -> LBS.ByteString -> ObjectDigest
+digestLazyByteString (DigestAlgorithm a) = fromDigest . hashLazyWith a
+
+-- | Check and convert a ByteString into an ObjectDigest
+fromByteString :: ByteArrayAccess ba => DigestAlgorithm -> ba -> Maybe ObjectDigest
+fromByteString (DigestAlgorithm a) = fmap fromDigest . digestFromByteStringWith a
+-- helper for above
+digestFromByteStringWith :: (HashAlgorithm a, ByteArrayAccess ba) => a -> ba -> Maybe (Digest a)
+digestFromByteStringWith _ = digestFromByteString
+
+-- | Convert an ObjectDigest to its hex representation
+-- TODO: probably more efficient if we can just coerce the converted Bytes..
+toHex :: ObjectDigest -> String
+toHex = T.unpack . decodeUtf8 . convertToBase Base16
+
+
+-- | Given the Text name of a digest algorithm, return a DigestAlgorithm
+-- | (or Nothing if we don't recognize the DigestAlgorithm's name).
+getDigestAlgorithm :: T.Text -> Maybe DigestAlgorithm
+getDigestAlgorithm "SHA256"     = Just $ DigestAlgorithm SHA256
+getDigestAlgorithm "SHA512"     = Just $ DigestAlgorithm SHA512
+getDigestAlgorithm _            = Nothing
---
+++ tests/Data/ContentStore/DigestSpec.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Data.ContentStore.DigestSpec(spec) where
+
+import Test.Hspec
+import Data.Maybe(fromJust)
+import Data.ContentStore.Digest
+import qualified Data.ByteString as B
+import qualified Data.ByteArray as BA
+
+testInput :: B.ByteString
+testInput = "The Dude abides."
+
+testSHA256Digest :: B.ByteString
+testSHA256Digest = "\xcd\x9c\xf3\x08\x62\x61\x24\xa7\x81\x81\xc0\x22\xfb\x9d\x0d\x51\x1a\xf1\xc1\x5d\x32\x20\x12\xf0\xa6\x9d\xfe\x86\x40\xc3\x40\xaa"
+
+testSHA256Hex :: String
+testSHA256Hex = "cd9cf308626124a78181c022fb9d0d511af1c15d322012f0a69dfe8640c340aa"
+
+spec :: Spec
+spec = do
+    describe "Digest.DigestAlgorithm" $ do
+        it "knows about SHA256" $
+            digestName <$> getDigestAlgorithm "SHA256" `shouldBe` Just "SHA256"
+
+        let daSHA256 = fromJust $ getDigestAlgorithm "SHA256"
+        it "has the correct size for SHA256" $
+            digestSize daSHA256 `shouldBe` 32
+
+        let od = digestByteString daSHA256 testInput
+        let hex = toHex od
+
+        it "does SHA256 correctly" $
+            BA.convert od `shouldBe` testSHA256Digest
+
+        it "converts ObjectDigest to hex" $
+            hex `shouldBe` testSHA256Hex
+
+    describe "Digest.DigestContext" $ do
+        let da = fromJust $ getDigestAlgorithm "SHA256"
+        let ctx = digestInit da
+        it "can do digests piece by piece" $ do
+            let hex = toHex $ digestFinalize $ digestUpdate ctx testInput
+            hex `shouldBe` testSHA256Hex
+
+    describe "fromByteString" $ do
+        let da = fromJust $ getDigestAlgorithm "SHA256"
+        let od = digestByteString da testInput
+        it "accepts a 32-byte ByteString" $
+            fromByteString da testSHA256Digest `shouldBe` Just od
+        it "rejects wrong-sized ByteString" $
+            fromByteString da (B.take 20 testSHA256Digest) `shouldBe` Nothing
