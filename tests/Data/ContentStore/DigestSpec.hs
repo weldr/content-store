d1c5735a10a001d3ac726e8b86f57cf521c58731
Chris Lumens <clumens@redhat.com>
2017-11-16 15:03:43 -0500

    Move SHA256 tests into their own spec function.
    There's about to be a whole lot more tests, so more organization will be
    helpful.

--- tests/Data/ContentStore/DigestSpec.hs
+++ tests/Data/ContentStore/DigestSpec.hs
@@ -11,14 +11,8 @@ import qualified Data.ByteArray as BA
 testInput :: B.ByteString
 testInput = "The Dude abides."
 
-testSHA256Digest :: B.ByteString
-testSHA256Digest = "\xcd\x9c\xf3\x08\x62\x61\x24\xa7\x81\x81\xc0\x22\xfb\x9d\x0d\x51\x1a\xf1\xc1\x5d\x32\x20\x12\xf0\xa6\x9d\xfe\x86\x40\xc3\x40\xaa"
-
-testSHA256Hex :: String
-testSHA256Hex = "cd9cf308626124a78181c022fb9d0d511af1c15d322012f0a69dfe8640c340aa"
-
-spec :: Spec
-spec = do
+specSHA256 :: Spec
+specSHA256 = do
     describe "Digest.DigestAlgorithm" $ do
         it "knows about SHA256" $
             digestName <$> getDigestAlgorithm "SHA256" `shouldBe` Just "SHA256"
@@ -31,22 +25,33 @@ spec = do
         let hex = toHex od
 
         it "does SHA256 correctly" $
-            BA.convert od `shouldBe` testSHA256Digest
+            BA.convert od `shouldBe` testDigest
 
         it "converts ObjectDigest to hex" $
-            hex `shouldBe` testSHA256Hex
+            hex `shouldBe` testHex
 
     describe "Digest.DigestContext" $ do
         let da = fromJust $ getDigestAlgorithm "SHA256"
         let ctx = digestInit da
         it "can do digests piece by piece" $ do
             let hex = toHex $ digestFinalize $ digestUpdate ctx testInput
-            hex `shouldBe` testSHA256Hex
+            hex `shouldBe` testHex
 
     describe "fromByteString" $ do
         let da = fromJust $ getDigestAlgorithm "SHA256"
         let od = digestByteString da testInput
         it "accepts a 32-byte ByteString" $
-            fromByteString da testSHA256Digest `shouldBe` Just od
+            fromByteString da testDigest `shouldBe` Just od
         it "rejects wrong-sized ByteString" $
-            fromByteString da (B.take 20 testSHA256Digest) `shouldBe` Nothing
+            fromByteString da (B.take 20 testDigest) `shouldBe` Nothing
+ where
+    testDigest :: B.ByteString
+    testDigest = "\xcd\x9c\xf3\x08\x62\x61\x24\xa7\x81\x81\xc0\x22\xfb\x9d\x0d\x51\x1a\xf1\xc1\x5d\x32\x20\x12\xf0\xa6\x9d\xfe\x86\x40\xc3\x40\xaa"
+
+    testHex :: String
+    testHex = "cd9cf308626124a78181c022fb9d0d511af1c15d322012f0a69dfe8640c340aa"
+
+spec :: Spec
+spec =
+    describe "SHA256"
+        specSHA256
