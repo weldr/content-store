b69596d1c129c059eec8bf5d0ee49cc49e0e86db
Chris Lumens <clumens@redhat.com>
2017-11-16 15:30:29 -0500

    Add tests for the various Blake digest algorithms.

--- tests/Data/ContentStore/DigestSpec.hs
+++ tests/Data/ContentStore/DigestSpec.hs
@@ -13,6 +13,156 @@ import qualified Data.ByteArray as BA
 testInput :: B.ByteString
 testInput = "The Dude abides."
 
+specBLAKE2 :: Spec
+specBLAKE2 =
+    describe "Digest.DigestAlgorithm" $ do
+        it "knows about BLAKE2" $
+            digestName <$> getDigestAlgorithm "BLAKE2" `shouldBe` Just "Blake2b_512"
+
+        let daBLAKE2 = fromJust $ getDigestAlgorithm "BLAKE2"
+        it "has the correct size for BLAKE2" $
+            digestSize daBLAKE2 `shouldBe` 64
+
+        let od = digestByteString daBLAKE2 testInput
+        let hex = toHex od
+
+        it "does BLAKE2 correctly" $
+            BA.convert od `shouldBe` testDigest
+
+        it "converts ObjectDigest to hex" $
+            hex `shouldBe` testHex
+ where
+    testDigest :: B.ByteString
+    testDigest = "\x0e\x5e\xf4\x9c\x6b\x5d\xf5\x6f\xca\x89\x9f\xef\xde\x64\x8e\xc6\xc5\x42\x95\xe4\x40\x68\x9d\xe9\xd0\x2c\xaf\xc4\x87\xa6\xdb\xac\x7c\x79\x60\x1e\x2d\xa7\xb3\x13\xc8\x52\xa7\xba\xdd\x1d\x31\x97\x22\xa9\x6b\xd0\xd9\x1c\x06\x75\x97\x4b\x07\x69\xbe\x12\x45\x94"
+
+    testHex :: String
+    testHex = "0e5ef49c6b5df56fca899fefde648ec6c54295e440689de9d02cafc487a6dbac7c79601e2da7b313c852a7badd1d319722a96bd0d91c0675974b0769be124594"
+
+specBLAKE2b :: Spec
+specBLAKE2b =
+    describe "Digest.DigestAlgorithm" $ do
+        it "knows about BLAKE2b" $
+            digestName <$> getDigestAlgorithm "BLAKE2b" `shouldBe` Just "Blake2b_512"
+
+        let daBLAKE2b = fromJust $ getDigestAlgorithm "BLAKE2b"
+        it "has the correct size for BLAKE2b" $
+            digestSize daBLAKE2b `shouldBe` 64
+
+        let od = digestByteString daBLAKE2b testInput
+        let hex = toHex od
+
+        it "does BLAKE2b correctly" $
+            BA.convert od `shouldBe` testDigest
+
+        it "converts ObjectDigest to hex" $
+            hex `shouldBe` testHex
+ where
+    testDigest :: B.ByteString
+    testDigest = "\x0e\x5e\xf4\x9c\x6b\x5d\xf5\x6f\xca\x89\x9f\xef\xde\x64\x8e\xc6\xc5\x42\x95\xe4\x40\x68\x9d\xe9\xd0\x2c\xaf\xc4\x87\xa6\xdb\xac\x7c\x79\x60\x1e\x2d\xa7\xb3\x13\xc8\x52\xa7\xba\xdd\x1d\x31\x97\x22\xa9\x6b\xd0\xd9\x1c\x06\x75\x97\x4b\x07\x69\xbe\x12\x45\x94"
+
+    testHex :: String
+    testHex = "0e5ef49c6b5df56fca899fefde648ec6c54295e440689de9d02cafc487a6dbac7c79601e2da7b313c852a7badd1d319722a96bd0d91c0675974b0769be124594"
+
+specBLAKE2b512 :: Spec
+specBLAKE2b512 =
+    describe "Digest.DigestAlgorithm" $ do
+        it "knows about BLAKE2b512" $
+            digestName <$> getDigestAlgorithm "BLAKE2b512" `shouldBe` Just "Blake2b_512"
+
+        let daBLAKE2b512 = fromJust $ getDigestAlgorithm "BLAKE2b512"
+        it "has the correct size for BLAKE2b512" $
+            digestSize daBLAKE2b512 `shouldBe` 64
+
+        let od = digestByteString daBLAKE2b512 testInput
+        let hex = toHex od
+
+        it "does BLAKE2b512 correctly" $
+            BA.convert od `shouldBe` testDigest
+
+        it "converts ObjectDigest to hex" $
+            hex `shouldBe` testHex
+ where
+    testDigest :: B.ByteString
+    testDigest = "\x0e\x5e\xf4\x9c\x6b\x5d\xf5\x6f\xca\x89\x9f\xef\xde\x64\x8e\xc6\xc5\x42\x95\xe4\x40\x68\x9d\xe9\xd0\x2c\xaf\xc4\x87\xa6\xdb\xac\x7c\x79\x60\x1e\x2d\xa7\xb3\x13\xc8\x52\xa7\xba\xdd\x1d\x31\x97\x22\xa9\x6b\xd0\xd9\x1c\x06\x75\x97\x4b\x07\x69\xbe\x12\x45\x94"
+
+    testHex :: String
+    testHex = "0e5ef49c6b5df56fca899fefde648ec6c54295e440689de9d02cafc487a6dbac7c79601e2da7b313c852a7badd1d319722a96bd0d91c0675974b0769be124594"
+
+specBLAKE2b256 :: Spec
+specBLAKE2b256 =
+    describe "Digest.DigestAlgorithm" $ do
+        it "knows about BLAKE2b256" $
+            digestName <$> getDigestAlgorithm "BLAKE2b256" `shouldBe` Just "Blake2b_256"
+
+        let daBLAKE2b256 = fromJust $ getDigestAlgorithm "BLAKE2b256"
+        it "has the correct size for BLAKE2b256" $
+            digestSize daBLAKE2b256 `shouldBe` 32
+
+        let od = digestByteString daBLAKE2b256 testInput
+        let hex = toHex od
+
+        it "does BLAKE2b256 correctly" $
+            BA.convert od `shouldBe` testDigest
+
+        it "converts ObjectDigest to hex" $
+            hex `shouldBe` testHex
+ where
+    testDigest :: B.ByteString
+    testDigest = "\x82\xe4\x69\x8a\x1e\xbe\x82\xed\xae\xc8\xe8\xa6\xdc\xf1\x8d\x57\xbb\x5a\xc6\x71\x8b\x6b\xf2\xbb\xc3\x8a\x00\xda\x2e\x29\x34\xe6"
+
+    testHex :: String
+    testHex = "82e4698a1ebe82edaec8e8a6dcf18d57bb5ac6718b6bf2bbc38a00da2e2934e6"
+
+specBLAKE2s :: Spec
+specBLAKE2s =
+    describe "Digest.DigestAlgorithm" $ do
+        it "knows about BLAKE2s" $
+            digestName <$> getDigestAlgorithm "BLAKE2s" `shouldBe` Just "Blake2s_256"
+
+        let daBLAKE2s = fromJust $ getDigestAlgorithm "BLAKE2s"
+        it "has the correct size for BLAKE2s" $
+            digestSize daBLAKE2s `shouldBe` 32
+
+        let od = digestByteString daBLAKE2s testInput
+        let hex = toHex od
+
+        it "does BLAKE2s correctly" $
+            BA.convert od `shouldBe` testDigest
+
+        it "converts ObjectDigest to hex" $
+            hex `shouldBe` testHex
+ where
+    testDigest :: B.ByteString
+    testDigest = "\xfe\x04\xd7\x14\x3e\x23\x1a\x36\xc7\xbd\x1f\x9b\xdb\x36\x04\x56\xdd\xe8\x1c\xb9\x22\x3a\xab\x25\x5f\x74\x9f\x80\xe9\x60\xd7\x9f"
+
+    testHex :: String
+    testHex = "fe04d7143e231a36c7bd1f9bdb360456dde81cb9223aab255f749f80e960d79f"
+
+specBLAKE2s256 :: Spec
+specBLAKE2s256 =
+    describe "Digest.DigestAlgorithm" $ do
+        it "knows about BLAKE2s256" $
+            digestName <$> getDigestAlgorithm "BLAKE2s256" `shouldBe` Just "Blake2s_256"
+
+        let daBLAKE2s256 = fromJust $ getDigestAlgorithm "BLAKE2s256"
+        it "has the correct size for BLAKE2s256" $
+            digestSize daBLAKE2s256 `shouldBe` 32
+
+        let od = digestByteString daBLAKE2s256 testInput
+        let hex = toHex od
+
+        it "does BLAKE2s256 correctly" $
+            BA.convert od `shouldBe` testDigest
+
+        it "converts ObjectDigest to hex" $
+            hex `shouldBe` testHex
+ where
+    testDigest :: B.ByteString
+    testDigest = "\xfe\x04\xd7\x14\x3e\x23\x1a\x36\xc7\xbd\x1f\x9b\xdb\x36\x04\x56\xdd\xe8\x1c\xb9\x22\x3a\xab\x25\x5f\x74\x9f\x80\xe9\x60\xd7\x9f"
+
+    testHex :: String
+    testHex = "fe04d7143e231a36c7bd1f9bdb360456dde81cb9223aab255f749f80e960d79f"
+
 specSHA256 :: Spec
 specSHA256 = do
     describe "Digest.DigestAlgorithm" $ do
@@ -100,3 +250,21 @@ spec = do
 
     describe "SHA512"
         specSHA512
+
+    describe "BLAKE2"
+        specBLAKE2
+
+    describe "BLAKE2b"
+        specBLAKE2b
+
+    describe "BLAKE2b512"
+        specBLAKE2b512
+
+    describe "BLAKE2b256"
+        specBLAKE2b256
+
+    describe "BLAKE2s"
+        specBLAKE2s
+
+    describe "BLAKE2s256"
+        specBLAKE2s256
